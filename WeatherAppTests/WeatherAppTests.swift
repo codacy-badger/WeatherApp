8bec62ab599117511bd1702bbe04b03596672743
emile <>
2019-06-13 11:38:50 +0300

    add test

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -7,20 +7,41 @@
 //
 
 import XCTest
+@testable import WeatherApp
 
 class WeatherAppTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+		guard let url = URL(string: "https:www.google.com/") else {
+			XCTAssertTrue(false, "Could not instantiate url")
+			return
+		}
+		var urlRequest = URLRequest(url: url)
+		let parameters: Parameters = ["UserID": 1,
+									  "Name": "Malcolm",
+									  "Email": "malcolm@network.com",
+									  "IsCool": true
+		]
+		
+		do {
+			let encoder = URLParameterEncoder()
+			try encoder.encode(urlRequest: &urlRequest, with: parameters)
+			guard let fullURL = urlRequest.url else {
+				XCTAssertTrue(false, "urlRequest url is nil.")
+				return
+			}
+			
+			let expectedURL = "https:www.google.com/?Name=Malcolm&Email=malcolm%2540network.com&UserID=1&IsCool=true"
+			XCTAssertEqual(fullURL.absoluteString.sorted(), expectedURL.sorted())
+		} catch {
+			
+		}
     }
 
     func testPerformanceExample() {
